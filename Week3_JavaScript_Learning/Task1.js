4e3b307640d8dc367d66f72c93190cc93277ecc6
Mohit <mohit96046@gmail.com>
2026-08-16 22:10:21 +0530

    Added WEEK 3 Tasks

---
+++ Week3_JavaScript_Learning/Task1.js
@@ -0,0 +1,39 @@
+//## ✅ Task 1: JavaScript Basics (15 Marks)
+
+// Create JavaScript programs demonstrating the basic concepts of JavaScript.
+
+// Your programs should include:
+
+// * JavaScript Syntax
+// * `console.log()`
+// * Single-line Comments
+// * Multi-line Comments
+// * Linking JavaScript with an HTML file
+// * Basic JavaScript Statements
+
+// Create simple examples to demonstrate how JavaScript code is written and executed.
+
+
+// Here Is The Starting
+
+// Javascript Syntax
+const name = "Mohit";
+let age = 20;
+console.log(`Name Is : ${name}\nAge Is : ${age}`)
+
+// console.log() -> Used To Display Data/Print Statements In TERMINAL
+console.log(`This Is Displayed On Terminal`)
+
+// THis Is Sinale LIne COMMENT
+
+/*This Is Line 1 of Multi Comment
+  This Is Line 2 of Multi Comment */
+
+// To  Link Javascript With HTML, We Provide Linking In HTML Body Tag 
+// This Link - <script src="task1.js"></script>.
+// Now, We Can Use Javascript In HTML
+
+//Basic Statements
+let clas = 12;
+const name1 = "Rohit";
+console.log(`Name Is : ${name1}\nClass Is ${clas}th`)
